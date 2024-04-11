afa7aac23e8f6e17e122d697cebf3ec826a56821
Extravi <98912029+Extravi@users.noreply.github.com>
2024-04-11 03:39:13 -0400

    Add files via upload

--- um/src/main.cpp
+++ um/src/main.cpp
@@ -17,10 +17,14 @@
 #include "./ImGui/imgui_impl_win32.h"
 #include "vector.h"
 #include "render.h"
+#include "font.h"
 
 // int width, int height
 int screenWidth = GetSystemMetrics(SM_CXSCREEN);
 int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+// global imgui menu
+bool showImGui = false;
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -285,10 +289,96 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
             UpdateWindow(overlay);
 
             ImGui::CreateContext();
-            ImGui::StyleColorsClassic();
+            ImGuiIO& io = ImGui::GetIO();
+            io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
 
             ImGui_ImplWin32_Init(overlay);
             ImGui_ImplDX11_Init(device, device_context);
+
+            // imgui theme
+            ImGuiStyle& style = ImGui::GetStyle();
+
+            style.Alpha = 1.0f;
+            style.WindowPadding = ImVec2(6.5f, 2.700000047683716f);
+            style.WindowRounding = 6.0f;
+            style.WindowBorderSize = 1.0f;
+            style.WindowMinSize = ImVec2(20.0f, 32.0f);
+            style.WindowTitleAlign = ImVec2(0.5f, 0.5f);
+            style.WindowMenuButtonPosition = ImGuiDir_None;
+            style.ChildRounding = 0.0f;
+            style.ChildBorderSize = 1.0f;
+            style.PopupRounding = 10.10000038146973f;
+            style.PopupBorderSize = 1.0f;
+            style.FramePadding = ImVec2(20.0f, 3.5f);
+            style.FrameRounding = 0.0f;
+            style.FrameBorderSize = 0.0f;
+            style.ItemSpacing = ImVec2(4.400000095367432f, 4.0f);
+            style.ItemInnerSpacing = ImVec2(4.599999904632568f, 3.599999904632568f);
+            style.IndentSpacing = 4.400000095367432f;
+            style.ColumnsMinSpacing = 5.400000095367432f;
+            style.ScrollbarSize = 8.800000190734863f;
+            style.ScrollbarRounding = 9.0f;
+            style.GrabMinSize = 9.399999618530273f;
+            style.GrabRounding = 0.0f;
+            style.TabRounding = 0.0f;
+            style.TabBorderSize = 0.0f;
+            style.ColorButtonPosition = ImGuiDir_Right;
+            style.ButtonTextAlign = ImVec2(0.5f, 0.5f);
+            style.SelectableTextAlign = ImVec2(0.0f, 0.0f);
+
+            style.Colors[ImGuiCol_Text] = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+            style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.4980392158031464f, 0.4980392158031464f, 0.4980392158031464f, 1.0f);
+            style.Colors[ImGuiCol_WindowBg] = ImVec4(0.05098039284348488f, 0.03529411926865578f, 0.03921568766236305f, 1.0f);
+            style.Colors[ImGuiCol_ChildBg] = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
+            style.Colors[ImGuiCol_PopupBg] = ImVec4(0.0784313753247261f, 0.0784313753247261f, 0.0784313753247261f, 1.0f);
+            style.Colors[ImGuiCol_Border] = ImVec4(0.1019607856869698f, 0.1019607856869698f, 0.1019607856869698f, 0.5f);
+            style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
+            style.Colors[ImGuiCol_FrameBg] = ImVec4(0.1607843190431595f, 0.1490196138620377f, 0.1921568661928177f, 1.0f);
+            style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_TitleBg] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.0f, 0.0f, 0.0f, 0.5099999904632568f);
+            style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.1372549086809158f, 0.1372549086809158f, 0.1372549086809158f, 1.0f);
+            style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.01960784383118153f, 0.01960784383118153f, 0.01960784383118153f, 0.5299999713897705f);
+            style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.3098039329051971f, 0.3098039329051971f, 0.3098039329051971f, 1.0f);
+            style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.407843142747879f, 0.407843142747879f, 0.407843142747879f, 1.0f);
+            style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.5098039507865906f, 0.5098039507865906f, 0.5098039507865906f, 1.0f);
+            style.Colors[ImGuiCol_CheckMark] = ImVec4(0.5450980663299561f, 0.4666666686534882f, 0.7176470756530762f, 1.0f);
+            style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_Button] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.3450980484485626f, 0.294117659330368f, 0.4588235318660736f, 1.0f);
+            style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.3137255012989044f, 0.2588235437870026f, 0.4274509847164154f, 1.0f);
+            style.Colors[ImGuiCol_Header] = ImVec4(0.3176470696926117f, 0.2784313857555389f, 0.407843142747879f, 1.0f);
+            style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.4156862795352936f, 0.364705890417099f, 0.529411792755127f, 1.0f);
+            style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.4039215743541718f, 0.3529411852359772f, 0.5098039507865906f, 1.0f);
+            style.Colors[ImGuiCol_Separator] = ImVec4(0.4274509847164154f, 0.4274509847164154f, 0.4980392158031464f, 0.5f);
+            style.Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_SeparatorActive] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_Tab] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_TabHovered] = ImVec4(0.3254902064800262f, 0.2862745225429535f, 0.4156862795352936f, 1.0f);
+            style.Colors[ImGuiCol_TabActive] = ImVec4(0.4000000059604645f, 0.3490196168422699f, 0.5058823823928833f, 1.0f);
+            style.Colors[ImGuiCol_TabUnfocused] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_PlotLines] = ImVec4(0.6078431606292725f, 0.6078431606292725f, 0.6078431606292725f, 1.0f);
+            style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.0f, 0.4274509847164154f, 0.3490196168422699f, 1.0f);
+            style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.8980392217636108f, 0.6980392336845398f, 0.0f, 1.0f);
+            style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.0f, 0.6000000238418579f, 0.0f, 1.0f);
+            style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.2588235437870026f, 0.5882353186607361f, 0.9764705896377563f, 0.3499999940395355f);
+            style.Colors[ImGuiCol_DragDropTarget] = ImVec4(1.0f, 1.0f, 0.0f, 0.8999999761581421f);
+            style.Colors[ImGuiCol_NavHighlight] = ImVec4(0.2784313857555389f, 0.250980406999588f, 0.3372549116611481f, 1.0f);
+            style.Colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.0f, 1.0f, 1.0f, 0.699999988079071f);
+            style.Colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.800000011920929f, 0.800000011920929f, 0.800000011920929f, 0.2000000029802322f);
+            style.Colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.800000011920929f, 0.800000011920929f, 0.800000011920929f, 0.3499999940395355f);
+
+            ImFontConfig font;
+            font.FontDataOwnedByAtlas = false;
+
+            io.Fonts->AddFontFromMemoryTTF((void*)rawData, sizeof(rawData), 17.0f, &font);
 
             bool running = true;
 
@@ -317,6 +407,49 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
                 ImGui_ImplDX11_NewFrame();
                 ImGui_ImplWin32_NewFrame();
                 ImGui::NewFrame();
+
+                const bool home_pressed = GetAsyncKeyState(VK_HOME);
+                if (home_pressed)
+                {
+                    showImGui = !showImGui;
+                    if (showImGui)
+                    {
+                        LONG_PTR exStyle = GetWindowLongPtr(overlay, GWL_EXSTYLE);
+                        exStyle &= ~WS_EX_TRANSPARENT; // remove WS_EX_TRANSPARENT
+                        SetWindowLongPtr(overlay, GWL_EXSTYLE, exStyle);
+                    }
+                    else
+                    {
+                        LONG_PTR exStyle = GetWindowLongPtr(overlay, GWL_EXSTYLE);
+                        exStyle |= WS_EX_TRANSPARENT; // add WS_EX_TRANSPARENT
+                        SetWindowLongPtr(overlay, GWL_EXSTYLE, exStyle);
+                    }
+                    Sleep(400);
+                }
+
+                // RGB
+                static int r = 255;
+                static int g = 0;
+                static int b = 255;
+
+                if (showImGui)
+                {
+                    ImGui::SetNextWindowSize(ImVec2(330, 300));
+                    ImGui::Begin("Extravi - GitHub", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+
+                    ImGui::Text("https://github.com/Extravi/cs2-kernel-esp");
+
+                    static ImVec4 color = ImVec4(r / 255.0f, g / 255.0f, b / 255.0f, 1.0f);
+                    ImGui::ColorEdit3("Enemy Color", (float*)&color);
+
+                    r = static_cast<int>(color.x * 255.0f);
+                    g = static_cast<int>(color.y * 255.0f);
+                    b = static_cast<int>(color.z * 255.0f);
+
+                    ImGui::End();
+                }
+
+                RGB enemy = { r, g, b };
 
                 for (int playerIndex = 1; playerIndex < 32; ++playerIndex) {
                     uintptr_t listentry = driver::read_memory<uintptr_t>(driver, entity_list + (8 * (playerIndex & 0x7FFF) >> 9) + 16);
@@ -363,14 +496,12 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
                     float height = screenPos.y - screenHead.y;
                     float width = height / 2.4f;
 
-                    RGB enmey = { 128, 0, 128 };
-
                     Render::DrawRect(
                         screenHead.x - width / 2,
                         screenHead.y,
                         width,
                         height,
-                        enmey,
+                        enemy,
                         1.5
                     );
                 }
